e24d4fc5e8ffca31af6f9ac1dd0f245f673dd0c8
Caleb <calebkiune@gmail.com>
2025-01-26 17:43:17 +0300

    adds updates

--- server/models.py
+++ server/models.py
@@ -19,11 +19,9 @@ class Restaurant(db.Model, SerializerMixin):
     name = db.Column(db.String)
     address = db.Column(db.String)
 
-    # Add relationship
     restaurant_pizzas = relationship('RestaurantPizza', backref='restaurant_ref', cascade="all, delete-orphan")
     pizzas = association_proxy('restaurant_pizzas', 'pizza')
 
-    # Add serialization rules
     serialize_rules = ('-restaurant_pizzas.restaurant_ref',)
 
     def to_dict(self):
@@ -44,11 +42,9 @@ class Pizza(db.Model, SerializerMixin):
     name = db.Column(db.String)
     ingredients = db.Column(db.String)
 
-    # Add relationship
     restaurant_pizzas = relationship('RestaurantPizza', backref='pizza_ref', cascade="all, delete-orphan")
     restaurants = association_proxy('restaurant_pizzas', 'restaurant')
 
-    # Add serialization rules
     serialize_rules = ('-restaurant_pizzas.pizza_ref',)
 
     def to_dict(self):
@@ -69,11 +65,9 @@ class RestaurantPizza(db.Model, SerializerMixin):
     restaurant_id = db.Column(db.Integer, ForeignKey('restaurants.id'))
     pizza_id = db.Column(db.Integer, ForeignKey('pizzas.id'))
 
-    # Add relationships
     restaurant = relationship('Restaurant', backref='restaurant_pizza_links', overlaps="restaurant_pizzas,restaurant_ref")
     pizza = relationship('Pizza', backref='pizza_pizza_links', overlaps="restaurant_pizzas,pizza_ref")
 
-    # Add serialization rules
     serialize_rules = ('-restaurant.restaurant_pizza_links', '-pizza.pizza_pizza_links')
 
     @validates('price')
@@ -88,8 +82,8 @@ class RestaurantPizza(db.Model, SerializerMixin):
             'price': self.price,
             'restaurant_id': self.restaurant_id,
             'pizza_id': self.pizza_id,
-            'restaurant': self.restaurant.to_dict() if self.restaurant else None,  # Check if restaurant is None
-            'pizza': self.pizza.to_dict() if self.pizza else None  # Check if pizza is None
+            'restaurant': self.restaurant.to_dict() if self.restaurant else None,
+            'pizza': self.pizza.to_dict() if self.pizza else None
         }
 
     def __repr__(self):
